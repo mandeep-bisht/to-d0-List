1fe971cfc11647031d1c44f3fe42f59c96ebc8b5
Mandeep Bisht <mandeepbisht@192.168.1.7>
2023-03-05 15:03:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Input from '@mui/material/Input';
+import AddCircleOutlineTwoToneIcon from '@mui/icons-material/AddCircleOutlineTwoTone';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import { useState } from 'react';
 
 function App() {
+  const ariaLabel = { 'aria-label': 'description' };
+  const [inputList, setInputList] = useState('');
+
+
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  }
+
+  const addClick = () => {
+    console.log("+ is clicked")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='alphaDiv'>
+      <div className='betaDiv'>
+        <h1 className='heading'>ToDo List</h1>
+        <div>
+          <Input placeholder="Add your Items" inputProps={ariaLabel} value={inputList} onChange={itemEvent} />
+          <AddCircleOutlineTwoToneIcon onClick={addClick} />
+        </div>
+        <div className='showItems'>
+          <ul>
+            <li> <HighlightOffIcon /> test item</li>
+            <li>test item</li>
+            <li>test item</li>
+            <li>test item</li>
+            
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
